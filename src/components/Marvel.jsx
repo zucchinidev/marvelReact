c0e97b9284840fce318514f1533fe04550e2ddd5
Andrea Zucchini <azga84@gmail.com>
2016-11-14 00:41:45 +0100

    Add button component to app

--- src/components/Marvel.jsx
+++ src/components/Marvel.jsx
@@ -1,3 +1,4 @@
+/* eslint no-console:0 */
 import React from 'react';
 import Logo from './Logo';
 import Card from './Card';
@@ -16,14 +17,21 @@ export default class MarvelApp extends React.Component {
     return APIKEY_QUERYSTRING;
   }
 
+  static getMarvelUrl(textToSearch) {
+    const characters = 'characters?nameStartsWith';
+    return `${MarvelApp.apiUrl}${characters}=${textToSearch}&${MarvelApp.apiKeyQueryString}`;
+  }
+
   constructor(...args) {
     super(...args);
     this.state = {
-      results: dummyData
+      results: dummyData,
+      isLoading: false
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSummit = this.handleSummit.bind(this);
   }
+
 
   handleChange({currentTarget}) {
     this.setState({textToSearch: currentTarget.value}, () => {
@@ -32,24 +40,31 @@ export default class MarvelApp extends React.Component {
 
   }
 
-
   handleSummit(event) {
     event.preventDefault();
+    const isLoading = true;
+    this.setState({isLoading});
     const {textToSearch} = this.state;
-    const url = `${MarvelApp.apiUrl}characters?nameStartsWith=${textToSearch}&${MarvelApp.apiKeyQueryString}`;
+    const url = MarvelApp.getMarvelUrl(textToSearch);
     fetch(url).then((response) => response.json())
-      .then((res) => {
-        this.setState({
-          results: res.data.results
-        });
-      });
+              .then((res) => {
+                this.setState({
+                  results: res.data.results,
+                  isLoading: false
+                });
+              });
   }
 
   render() {
+    const {isLoading} = this.state;
     return (
       <div className='container'>
         <Logo isCentered/>
-        <SearchForm handleChange={this.handleChange} handleSummit={this.handleSummit}/>
+        <SearchForm
+          handleChange={this.handleChange}
+          handleSummit={this.handleSummit}
+          isLoading={isLoading}
+        />
         <div className='results'>
           {this.state.results.map((hero) => <Card item={hero} key={hero.id}/>)}
         </div>
