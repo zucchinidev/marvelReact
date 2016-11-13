0adbb21d1a4baa313436a6acf502df19caf2f697
Andrea Zucchini <azga84@gmail.com>
2016-11-14 00:42:05 +0100

    Insert button component in form

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,21 +1,29 @@
 import React, {PropTypes} from 'react';
+import Button from './Button';
 import './SearchForm.scss';
 
 export default function SearchForm({
   handleSummit,
-  handleChange
+  handleChange,
+  isLoading
 }) {
   return (
     <form className='form--element' onSubmit={handleSummit}>
-      <input className='search--input' type='text' onChange={handleChange}/>
-      <button className='btn--default'>
-        Search
-      </button>
+      <input
+        className='search--input'
+        disabled={isLoading}
+        type='text'
+        onChange={handleChange}
+      />
+      <Button
+        isLoading={isLoading}
+      />
     </form>
   );
 }
 
 SearchForm.propTypes = {
   handleSummit: PropTypes.func.isRequired,
-  handleChange: PropTypes.func.isRequired
+  handleChange: PropTypes.func.isRequired,
+  isLoading: PropTypes.bool.isRequired
 };
