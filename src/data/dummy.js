66d3ecafc5619196a3b6a630592b7217bd416b9f
Andrea Zucchini <azga84@gmail.com>
2016-11-13 23:30:45 +0100

    create dummy data

---
+++ src/data/dummy.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    'id': 1017575,
+    'name': 'Captain America (Sam Wilson)',
+    'description': '<p>Sam Wilson, a Harlem native, previously operated under the name Falcon, fighting alongside the Avengers. When his longtime friend Steve Rogers stepped down as Captain America, Wilson was hand-picked to fill the role. Now the former Falcon soars through the skies wearing red, white and blue.</p>',
+    'thumbnail': {'path': 'http://i.annihil.us/u/prod/marvel/i/mg/c/80/545a84a75ddaf', 'extension': 'jpg'}
+  },
+  {
+    'id': 1010913,
+    'name': 'Captain America (Ultimate)',
+    'description': '',
+    'thumbnail': {'path': 'http://i.annihil.us/u/prod/marvel/i/mg/5/80/53176a0bb810c', 'extension': 'jpg'},
+  },
+  {
+    'id': 1017105,
+    'name': 'Captain America/Steve Rogers (MAA)',
+    'description': 'During World War II, Steve Rogers enlisted in the military and was injected with a super-serum that turned him into super-soldier Captain America! Hes a skilled strategist and even more skilled with his shield, but its his courage and good heart that makes Captain America both a leader and a true hero. ',
+    'thumbnail': {
+      'path': 'http://i.annihil.us/u/prod/marvel/i/mg/3/10/52321928eaa72', 'extension': 'jpg'
+    }
+  }
+];
